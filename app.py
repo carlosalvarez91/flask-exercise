b0885bbf31c8299283ab95aa416c94b70ff3c231
carlos <bobcn_1991@hotmail.com>
2017-10-18 09:18:22 +0100

    Toggle Desc Asc  ( CLEAN IT if time! )

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_csv():
     with open('data.csv', 'rb') as csv_file:
         csv_obj = csv.DictReader(csv_file) # here i suppose to replace white spaces for '_' and remove the '[]' in order to call each row at the frontend; if I cant, Im gonna change the data.csv, Property Name => Property_Name and [1] => 1
         srt = sorted(csv_obj, key=lambda row: row, reverse=True)#list sorted by lease amount in ascending order.
-        csv_list = list(srt)
+        csv_list = list(srt)[:5]# [:5] display the first 5 items
     return csv_list
 
 @app.route('/')
@@ -22,6 +22,15 @@ def index():
     template = 'home.html'
     object_list =  get_csv()
     return render_template(template, object_list=object_list)
+
+@app.route("/toggle/", methods=['POST'])
+def move_forward():
+    with open('data.csv', 'rb') as csv_file:
+        csv_obj = csv.DictReader(csv_file) # here i suppose to replace white spaces for '_' and remove the '[]' in order to call each row at the frontend; if I cant, Im gonna change the data.csv, Property Name => Property_Name and [1] => 1
+        srt = sorted(csv_obj, key=lambda row: row, reverse=False)#list sorted by lease amount in ascending order.
+        object_list = list(srt)[:5]# [:5] display the first 5 items
+    return render_template('home.html', object_list=object_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
